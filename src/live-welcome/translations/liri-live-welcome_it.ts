242f4c8fa141071ffc4604ab6d49d55f1b6bee23
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2019-06-12 15:34:41 -0400

    Add Italian translation of live welcome

---
+++ src/live-welcome/translations/liri-live-welcome_it.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="it" version="2.1">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="31"/>
+        <location filename="../qml/main.qml" line="47"/>
+        <source>Welcome to Liri OS</source>
+        <translation>Benvenuto a Liri OS</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="48"/>
+        <source>You are currently running Liri OS from live media.
+You can install Liri OS now, or launch &quot;Install to Hard Drive&quot; later.</source>
+        <translation>Stai utilizzando Liri OS da una live.
+Puoi installare Liri OS adesso, oppure lanciare &quot;Installa su disco&quot; piú tardi.</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="56"/>
+        <source>Try Liri OS</source>
+        <translation>Prova Liri OS</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="62"/>
+        <source>Install to Hard Drive</source>
+        <translation>Installa su disco</translation>
+    </message>
+</context>
+</TS>
